72ab7e91751577fad3d3426d5e753a6ffb518c9b
avigdor <avigtoli1991@gmai.com>
2024-02-08 05:05:46 +0200

    fix reg and log screen

--- todo-list/api/index.js
+++ todo-list/api/index.js
@@ -1,17 +1,17 @@
-const express = require("express"); // הסביבה איתה עובדים לצורך טיפול בבקשות כמו 'GET' ו 'POST' 
-const bodyParser = require("body-parser"); // חילוץ המידע מהסביבה 
+const express = require("express"); // הסביבה איתה עובדים לצורך טיפול בבקשות כמו 'GET' ו 'POST'
+const bodyParser = require("body-parser"); // חילוץ המידע מהסביבה
 const mongoose = require("mongoose"); // המאגר נתונים שלנו שאיתו ה express יעבוד
 const crypto = require("crypto"); // מודל ההצפנה של נוד ג'יי אסס לצורך הצפנת הסיסמאות וכדומא
 
 const app = express();
 const port = 3000;
-const cors = require("cors"); // על מנת לאבטח שהשרת יגיב כראוי מבקשות צולבות מכל מיני מקורות CORS ייבוא תוכנת ביינים 
+const cors = require("cors"); // על מנת לאבטח שהשרת יגיב כראוי מבקשות צולבות מכל מיני מקורות CORS ייבוא תוכנת ביינים
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose      //התחברות למאגר הדטה בייס
+mongoose //התחברות למאגר הדטה בייס
   .connect(
     "mongodb+srv://avigtoli1991:aXHAjHza0Df5EKRp@cluster0.vbn2ikb.mongodb.net/"
   )
@@ -22,63 +22,62 @@ mongoose      //התחברות למאגר הדטה בייס
     console.log("Error connectin to MongoDB!", error);
   });
 
-app.listen(port, () => {   // בקשה מהסביבה בה אנו משתמשים להאזין לפורט שלנו בשרת
+app.listen(port, () => {
+  // בקשה מהסביבה בה אנו משתמשים להאזין לפורט שלנו בשרת
   console.log("Server is running on port 3000");
 });
 
-const User = require('./models/user');
-const todo = require('./models/todo');
+const User = require("./models/user");
+const todo = require("./models/todo");
 const jwt = require("jsonwebtoken");
 
-app.post("/register",async(req,res)=>{
-  try{
-    const {name,email,password}=req.body;
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    //check if user is already registred 
-    const existingUser = await User.findOne({email});
-    if (existingUser){
+    //check if user is already registred
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
       console.log("Email already registered");
     }
 
     const newUser = new User({
       name,
       email,
-      password
+      password,
     });
     await newUser.save();
-    res.status(202).json({message: "User registered successfully"})
-  }catch(error){
+    res.status(202).json({ message: "User registered successfully" });
+  } catch (error) {
     console.log("Error registring the user", error);
-    res.status(500).json({message: "Registration failed"})
+    res.status(500).json({ message: "Registration failed" });
   }
-})
-const generateSecretKey = () =>{
+});
+const generateSecretKey = () => {
   secretKey = crypto.randomBytes(32).toString("hex");
   return secretKey;
-}
+};
 
 const secretKey = generateSecretKey();
 
-app.post("/login", async(req,res)=>{
- 
-  try{
-    const {email,password} = req.body;
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
     //check if user is existing
-    const user = await User.findOne({email});
-    if (!user){
-      return res.status(401).json({message:"Invalid email"});
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
     }
-    if(user.password !== password){
-      return res.status(401).json({message:"Invalid password"});
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid password" });
     }
 
-    const token = jwt.sign({userId:user._id},secretKey);
+    const token = jwt.sign({ userId: user._id }, secretKey);
 
-    res.status(200).json({token});
-  }catch(error){
-    console.log("Login failed",error);
-    res.status(500).json({message:"Login failed"});
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Login failed", error);
+    res.status(500).json({ message: "Login failed" });
   }
-  
 });
